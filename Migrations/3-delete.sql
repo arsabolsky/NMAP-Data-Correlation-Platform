bc71efe1018e4b70d6af0ad715166754fdc1fbc2
arsabolsky <11062264+arsabolsky@users.noreply.github.com>
2026-02-27 23:11:28 +0000

    Disable and Enable FOREIGN_KEY_CHECKS

--- Migrations/3-delete.sql
+++ Migrations/3-delete.sql
@@ -1,3 +1,4 @@
+SET FOREIGN_KEY_CHECKS=0; -- to disable foreign key check
 -- 1. Delete from COMPANY (Base table)
 DELETE FROM COMPANY;
 
@@ -9,3 +10,5 @@ DELETE FROM EMPLOYEE;
 
 -- 4. Delete from SCAN (Depends on LOCATION)
 DELETE FROM SCAN;
+
+SET FOREIGN_KEY_CHECKS=1; -- to re-enable foreign key check
